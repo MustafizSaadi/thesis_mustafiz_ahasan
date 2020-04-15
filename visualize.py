442db398161cec376548e89699bee7fb26acbfe2
MustafizSaadi <mustafizur.rahman.1167141@gmail.com>
2020-04-15 07:26:17 -0400

    added legend

--- visualize.py
+++ visualize.py
@@ -1,23 +1,33 @@
 import matplotlib.pyplot as plt
 import numpy as np
 #f = open("successVSagents","r")
-f = open("runtimeVSagents","r")
-x = np.zeros(5)
-y1 = np.zeros(5)
-y2 = np.zeros(5)
+#f = open("runtimeVSagents","r")
+f = open("costVSagents","r")
+x = np.zeros(8)
+y1 = np.zeros(8)
+y2 = np.zeros(8)
+#y3 = np.zeros(6)
 
-#agents,CBS,CBSH
+#agents,CBS,CBSH,pibt
 i = 0
 for s in f:
 	inp = s.split(',')
 	x[i] = float(inp[0])
 	y1[i] = float(inp[1])
 	y2[i] = float(inp[2])
+#	y3[i] = float(inp[3])
 	i += 1
 
-plt.plot(x,y1)
-plt.plot(x,y2)
+fig,ax = plt.subplots()
+
+ax.plot(x,y1,label='CBS(ID)')
+#ax.plot(x,y2,label='CBSH-WDG')
+ax.plot(x,y2,label='PIBT')
+
+
+ax.legend()
+
 plt.xlabel("#Agents")
-#plt.ylabel("success(ms)")
-plt.ylabel("runtime(ms)")
+#plt.ylabel("runtime(ms)")
+plt.ylabel("Sum of costs")
 plt.show()
